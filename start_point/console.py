c1c5bed6dc12ba0108ff89d49d8ce2edf0a84e39
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-04-19 16:07:51 +0100

    Add function to delete all albums

--- start_point/console.py
+++ start_point/console.py
@@ -4,6 +4,9 @@ from models.artist import Artist
 import repositories.album_repository as album_repository
 import repositories.artist_repository as artist_repository
 
+album_repository.delete_all()
+artist_repository.delete_all()
+
 artist_1 = Artist("Freddy Mercury")
 artist_repository.save(artist_1)
 
