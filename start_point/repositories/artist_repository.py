2b06d0744c17da826863be4c1081df5fa4420b11
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-04-19 15:38:23 +0100

    Add function save for artist

--- start_point/repositories/artist_repository.py
+++ start_point/repositories/artist_repository.py
@@ -3,4 +3,9 @@ from db.run_sql import run_sql
 from models.artist import Artist
 
 def save(artist):
-    sql = "INSERT INTO artist (name)"
+    sql = "INSERT INTO artist (name) VALUES (%s) RETURNING *"
+    values = [artist.name]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    artist.id = id
+    return artist
