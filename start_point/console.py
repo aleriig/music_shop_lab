c0f01354f77b915fe02503af1b0ad1d3f89a4351
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-04-19 16:01:33 +0100

    Add function to save an album

--- start_point/console.py
+++ start_point/console.py
@@ -1,8 +1,11 @@
-# from models.album import Album
+from models.album import Album
 from models.artist import Artist
 
-# import repositories.album_repository as album_repository
+import repositories.album_repository as album_repository
 import repositories.artist_repository as artist_repository
 
 artist_1 = Artist("Freddy Mercury")
 artist_repository.save(artist_1)
+
+album_1 = Album("Album 1", "Rock", artist_1)
+album_repository.save(album_1)
