d5c044ffb2a5aad3162e9eb42cd116e513db2e5f
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-04-19 16:21:32 +0100

    Add function to show all the albums in the db

--- start_point/repositories/album_repository.py
+++ start_point/repositories/album_repository.py
@@ -3,6 +3,8 @@ from db.run_sql import run_sql
 from models.artist import Artist
 from models.album import Album
 
+import repositories.artist_repository as artist_repository
+
 def save(album):
     sql = "INSERT INTO albums (title, genre, artist_id) VALUES (%s, %s, %s) RETURNING *"
     values = [album.title, album.genre, album.artist.id]
@@ -14,3 +16,16 @@ def save(album):
 def delete_all():
     sql = "DELETE FROM albums"
     run_sql(sql)
+
+def select(id):
+    album = None
+
+    sql = "SELECT * FROM albums WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+
+    if result is not None:
+        artist = artist_repository.select(result['artist_id'])
+        album = Album(result['title'], result['genre'], artist)
+
+    return album
